22202b9741f9fd25e60e89794e547d495e31cb92
y2meta <y2meta@proton.me>
2023-05-07 20:38:59 +0530

    ht access changes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 exports.onClientEntry = () => {
-    if (window.location.pathname === "/") {
-      window.location.replace("/youtube-to-mp3/");
+    if (window.location.pathname === "/youtube-to-mp3/") {
+      window.location.replace("/youtube-to-mp4/");
     }
   };
   
