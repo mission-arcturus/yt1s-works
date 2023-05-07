9e197ad7857c97ec2f14ee9702eb11c3c2e4ebd0
y2meta <y2meta@proton.me>
2023-05-07 10:19:45 +0530

    fix: lang changes

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,8 +2,10 @@ exports.createPages = async ({ actions }) => {
     const { createRedirect } = actions
   
     createRedirect({
-      fromPath: `https://yt1s.works/en/youtube-to-mp4/`,
-      toPath: `https://yt1s.works/youtube-to-mp4/`,
+      fromPath: `/en/youtube-to-mp4/`,
+      isPermanent: true,
+      redirectInBrowser: true,
+      toPath: `/youtube-to-mp4/`,
       statusCode: 301,
     })
   }
