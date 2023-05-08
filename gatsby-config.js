8ad418b8cef19dee7491d7cf840143f356e06b74
y2meta <y2meta@proton.me>
2023-05-08 09:21:05 +0530

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,25 +68,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-netlify`,
       options: {
-        allPageHeaders: [
-          `X-Content-Type-Options: nosniff`,
-          `X-Frame-Options: DENY`,
-          `Referrer-Policy: same-origin`,
-          `Strict-Transport-Security: max-age=31536000; includeSubDomains; preload`,
-          `cache-control: public, max-age=31536000, immutable`,
-        ],
-        mergeSecurityHeaders: true,
-        mergeLinkHeaders: true,
-        mergeCachingHeaders: true,
-        transformHeaders: (headers, path) => {
-          if (path === "/youtube-to-mp4/") {
-            return {
-              "cache-control": "public, max-age=0, must-revalidate",
-              location: "/youtube-to-mp3/",
-            };
-          }
-
-          return headers;
+        headers: {
+          "/youtube-to-mp4/": [
+            "cache-control: public, max-age=0, must-revalidate",
+            "location: /youtube-to-mp3/",
+          ],
         },
       },
     },
